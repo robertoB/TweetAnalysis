72c6bb57f1247fbeeee9e64aafca97980cac2334
robertoB <robertobitto@gmail.com>
2016-02-14 23:57:36 +0100

    Create Train.java

---
+++ Train.java
@@ -0,0 +1,13 @@
+package TweetMicheleRoberto;
+
+import edu.stanford.nlp.sentiment.SentimentTraining;
+
+public class Train {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		   String [] argo= {"-numHid", "25", "-trainPath", "train.txt", "-devPath", "dev.txt", "-train", "-model", "english.ser.gz"};
+//	       SentimentTraining.main(argo);
+	}
+
+}
